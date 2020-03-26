abc6d1810cd225fbff8e097d30ce6a9611034c94
Oaz <github@azeau.com>
2020-03-26 15:46:08 +0100

    remove obsolete model

--- sample/Model.cs
+++ sample/Model.cs
@@ -89,29 +89,11 @@ namespace AvaloniaGraphControlSample
   class Model
   {
     public IEnumerable<NamedGraph> SampleGraphs => new NamedGraph[] {
-      new NamedGraph {Name="Simple Graph", Graph=SimpleGraph, Definition=SimpleGraphEdges},
-      new NamedGraph {Name="Simple Interactive Graph", Graph=SimpleInteractiveGraph, Definition=SimpleInteractiveGraphEdges},
-      new NamedGraph {Name="Family Tree", Graph=FamilyTree, Definition=FamilyTreeEdges},
-      new NamedGraph {Name="State Machine", Graph=StateMachine, Definition=StateMachineEdges}
+      new NamedGraph {Name="Simple Graph", Definition=SimpleGraphEdges},
+      new NamedGraph {Name="Simple Interactive Graph", Definition=SimpleInteractiveGraphEdges},
+      new NamedGraph {Name="Family Tree", Definition=FamilyTreeEdges},
+      new NamedGraph {Name="State Machine", Definition=StateMachineEdges}
     };
-    public static Graph SimpleGraph
-    {
-      get
-      {
-        var graph = new Microsoft.Msagl.Drawing.Graph("graph");
-        graph.RootSubgraph.IsVisible = false;
-        graph.AddEdge("A", "B");
-        graph.AddEdge("A", "D");
-        graph.AddEdge("A", "E");
-        graph.AddEdge("B", "C");
-        graph.AddEdge("B", "D");
-        graph.AddEdge("D", "A");
-        graph.AddEdge("D", "E");
-        foreach (var node in graph.Nodes)
-          node.UserData = new StandardItem(node.Id);
-        return graph;
-      }
-    }
 
     public static (IEnumerable<AvaloniaGraphControl.Edge>, Func<object, object>) SimpleGraphEdges
     {
@@ -134,18 +116,6 @@ namespace AvaloniaGraphControlSample
       }
     }
 
-    public static Graph SimpleInteractiveGraph
-    {
-      get
-      {
-        var graph = SimpleGraph;
-        foreach (var node in graph.Nodes)
-          node.UserData = new InteractiveItem(node.Id);
-        return graph;
-      }
-    }
-
-
     public static (IEnumerable<AvaloniaGraphControl.Edge>, Func<object, object>) SimpleInteractiveGraphEdges
     {
       get
@@ -166,56 +136,6 @@ namespace AvaloniaGraphControlSample
         return (edges, _ => null);
       }
     }
-
-    public static Graph FamilyTree
-    {
-      get
-      {
-        var graph = new Microsoft.Msagl.Drawing.Graph("graph");
-        graph.RootSubgraph.IsVisible = false;
-        graph.Attr.LayerDirection = LayerDirection.BT;
-        graph.AddEdge("f1", "Abraham");
-        graph.AddEdge("f1", "Mona");
-        graph.AddEdge("Homer", "f1");
-        graph.AddEdge("f2", "Clancy");
-        graph.AddEdge("f2", "Jackie");
-        graph.AddEdge("Marge", "f2");
-        graph.AddEdge("Patty", "f2");
-        graph.AddEdge("Selma", "f2");
-        graph.AddEdge("f3", "Homer");
-        graph.AddEdge("f3", "Marge");
-        graph.AddEdge("Bart", "f3");
-        graph.AddEdge("Lisa", "f3");
-        graph.AddEdge("Maggie", "f3");
-        graph.AddEdge("Ling", "Selma");
-        foreach (var node in graph.Nodes)
-        {
-          if (node.LabelText.StartsWith("f"))
-          {
-            node.LabelText = string.Empty;
-            node.Attr.FillColor = Microsoft.Msagl.Drawing.Color.Gray;
-          }
-        }
-        graph.FindNode("Abraham").UserData = new Male("Abraham", "https://en.wikipedia.org/wiki/Grampa_Simpson");
-        graph.FindNode("Mona").UserData = new Female("Mona", "https://en.wikipedia.org/wiki/Mona_Simpson_(The_Simpsons)");
-        graph.FindNode("Homer").UserData = new Male("Homer", "https://en.wikipedia.org/wiki/Homer_Simpson");
-        graph.FindNode("Clancy").UserData = new Male("Clancy", "https://en.wikipedia.org/wiki/Simpson_family#Clancy_Bouvier");
-        graph.FindNode("Jackie").UserData = new Female("Jackie", "https://en.wikipedia.org/wiki/Simpson_family#Jacqueline_Bouvier");
-        graph.FindNode("Marge").UserData = new Female("Marge", "https://en.wikipedia.org/wiki/Marge_Simpson");
-        graph.FindNode("Patty").UserData = new Female("Patty", "https://en.wikipedia.org/wiki/Patty_and_Selma");
-        graph.FindNode("Selma").UserData = new Female("Selma", "https://en.wikipedia.org/wiki/Patty_and_Selma");
-        graph.FindNode("Ling").UserData = new Female("Ling", "https://en.wikipedia.org/wiki/Simpson_family#Ling_Bouvier");
-        graph.FindNode("Bart").UserData = new Male("Bart", "https://en.wikipedia.org/wiki/Bart_Simpson");
-        graph.FindNode("Lisa").UserData = new Female("Lisa", "https://en.wikipedia.org/wiki/Lisa_Simpson");
-        graph.FindNode("Maggie").UserData = new Female("Maggie", "https://en.wikipedia.org/wiki/Maggie_Simpson");
-        foreach (var edge in graph.Edges)
-        {
-          edge.Attr.ArrowheadAtTarget = ArrowStyle.None;
-        }
-        return graph;
-      }
-    }
-
 
     public static (IEnumerable<AvaloniaGraphControl.Edge>, Func<object, object>) FamilyTreeEdges
     {
@@ -255,82 +175,6 @@ namespace AvaloniaGraphControlSample
         edges.Add(CreateEdge(f3, maggie));
         edges.Add(CreateEdge(selma, ling));
         return (edges, _ => null);
-      }
-    }
-
-    public static Graph StateMachine
-    {
-      get
-      {
-        var graph = new Microsoft.Msagl.Drawing.Graph("graph");
-        graph.RootSubgraph.IsVisible = false;
-
-        var mainGraph = new Subgraph("State Machine");
-        mainGraph.Attr.FillColor = Microsoft.Msagl.Drawing.Color.LightGray;
-        graph.RootSubgraph.AddSubgraph(mainGraph);
-
-        var onGraph = new Subgraph("On");
-        onGraph.Attr.FillColor = Microsoft.Msagl.Drawing.Color.LightSkyBlue;
-        mainGraph.AddSubgraph(onGraph);
-
-        var availableGraph = new Subgraph("Available");
-        availableGraph.Attr.FillColor = Microsoft.Msagl.Drawing.Color.LightGreen;
-        onGraph.AddSubgraph(availableGraph);
-
-        void SetInitialNode(Subgraph sugbgraph, string nodeID)
-        {
-          With(graph.FindNode(nodeID), n =>
-          {
-            sugbgraph.AddNode(n);
-            n.Label.Text = string.Empty;
-            n.Label.IsVisible = false;
-            n.Attr.FillColor = Microsoft.Msagl.Drawing.Color.Black;
-            n.Attr.Shape = Shape.Circle;
-          });
-        }
-        void SetRegularNode(Subgraph subgraph, Microsoft.Msagl.Drawing.Color color, string nodeID)
-        {
-          With(graph.FindNode(nodeID), n =>
-          {
-            subgraph.AddNode(n);
-            n.Attr.FillColor = color;
-          });
-        }
-
-        graph.AddEdge("initAvailable", "Paused");
-        graph.AddEdge("Paused", "Starting").LabelText = "Start";
-        graph.AddEdge("Starting", "Started").LabelText = "StartingComplete";
-        graph.AddEdge("Started", "Pausing").LabelText = "Pause";
-        graph.AddEdge("Pausing", "Paused").LabelText = "PausingComplete";
-        SetInitialNode(availableGraph, "initAvailable");
-        void SetAvailableNode(string nodeID) => SetRegularNode(availableGraph, Microsoft.Msagl.Drawing.Color.Yellow, nodeID);
-        SetAvailableNode("Paused");
-        SetAvailableNode("Starting");
-        SetAvailableNode("Started");
-        SetAvailableNode("Pausing");
-
-        graph.AddEdge("initOn", availableGraph.Id);
-        graph.AddEdge(availableGraph.Id, "Failure").LabelText = "FailureDetected";
-        graph.AddEdge("Failure", availableGraph.Id).LabelText = "Reset";
-        SetInitialNode(onGraph, "initOn");
-        void SetOnNode(string nodeID) => SetRegularNode(onGraph, Microsoft.Msagl.Drawing.Color.LimeGreen, nodeID);
-        SetOnNode("Failure");
-
-        graph.AddEdge("initMain", "Off");
-        graph.AddEdge("Off", onGraph.Id).LabelText = "SwitchOn";
-        graph.AddEdge(onGraph.Id, "Cleaning").LabelText = "SwitchOff";
-        graph.AddEdge("Cleaning", "Off").LabelText = "CleaningComplete";
-        SetInitialNode(mainGraph, "initMain");
-        void SetMainNode(string nodeID) => SetRegularNode(mainGraph, Microsoft.Msagl.Drawing.Color.CornflowerBlue, nodeID);
-        SetMainNode("Off");
-        SetMainNode("Cleaning");
-
-        foreach (var edge in graph.Edges)
-        {
-          if (edge.Label != null)
-            edge.Label.FontSize = 6;
-        }
-        return graph;
       }
     }
 
