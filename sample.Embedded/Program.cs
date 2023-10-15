d893149b55a316c3a87f44eb272d12187e02dccf
Olivier Azeau <git@azeau.com>
2023-10-15 22:55:33 +0200

    add Embedded linux sample

---
+++ sample.Embedded/Program.cs
@@ -0,0 +1,28 @@
+﻿using Avalonia;
+using Avalonia.Media;
+
+namespace AvaloniaGraphControlSample.Embedded;
+
+class Program
+{
+  [STAThread]
+  public static void Main(string[] args)
+  {
+    BuildAvaloniaApp().StartLinuxFbDev(args);
+  }
+  
+  public static AppBuilder BuildAvaloniaApp()
+    => AppBuilder.Configure<App>()
+      .WithInterFont()
+      .With(new FontManagerOptions
+      {
+        DefaultFamilyName = "avares://Avalonia.Fonts.Inter/Assets#Inter"
+      })
+      .LogToTrace()
+      .SetUrlOpener(OpenUrl);
+  
+  private static void OpenUrl(string url)
+  {
+    Console.WriteLine($"Open {url}");
+  }
+}
